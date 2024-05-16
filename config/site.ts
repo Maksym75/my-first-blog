36edcbc1d42c5dae9f286686558f794b66a7c302
MaksymKolii <maksymkolii@gmail.com>
2024-05-16 16:10:52 +0200

    Site-Header

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+	name: 'MadMaxBlog',
+	url: 'https://example.com',
+	description: 'Nextjs 14 blog using velite, tailwind and shadcn',
+	author: 'MaksymKolii',
+	links: {
+		twitter: 'https://twitter.com/KoliiMaksym',
+		github: 'https://github.com/Maksym75',
+		personalSite: '', // https://jamesshopland.com
+	},
+}
+
+export type SiteConfig = typeof siteConfig
