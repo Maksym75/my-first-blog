bc10d46e4ea46c2edb3e71f8c4a9145525f09c1c
MaksymKolii <maksymkolii@gmail.com>
2024-06-10 18:32:26 +0200

    Changed image OG

--- config/site.ts
+++ config/site.ts
@@ -1,12 +1,12 @@
 export const siteConfig = {
 	name: 'MadMaxBlog',
-	url: 'https://example.com',
+	url: 'https://my-first-blog-d0w7w4fzj-maksyms-projects-a3565e8f.vercel.app',
 	description: 'Nextjs 14 blog using velite, tailwind and shadcn',
 	author: 'MaksymKolii',
 	links: {
 		twitter: 'https://twitter.com/KoliiMaksym',
 		github: 'https://github.com/Maksym75',
-		personalSite: '', // https://jamesshopland.com
+		personalSite: '', 
 	},
 }
 
